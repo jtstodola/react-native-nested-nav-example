b0065ae63e08b09abc13075e7891b93e912b1323
Jess Stodola <jtstodola@gmail.com>
2021-01-29 15:58:19 -0600

    Add images

---
+++ app/assets/images/index.ts
@@ -0,0 +1,11 @@
+import background1 from './background1.jpg'
+import background2 from './background2.jpg'
+import background3 from './background3.jpg'
+
+const images = {
+  background1,
+  background2,
+  background3,
+}
+
+export default images
