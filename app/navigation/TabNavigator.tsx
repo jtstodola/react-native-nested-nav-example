f3838de41899ac9870566d6f96c56698de039749
Jess Stodola <jtstodola@gmail.com>
2021-02-06 20:03:21 -0600

    Add icons and nav to tab navigator

--- app/navigation/TabNavigator.tsx
+++ app/navigation/TabNavigator.tsx
@@ -1,18 +1,119 @@
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
-import { Home, Thing1, Thing2, Settings } from '../components'
+import {
+  Home,
+  Terminology,
+  Boats,
+  BoatInformation,
+  Settings,
+} from '../components'
+import { createStackNavigator } from '@react-navigation/stack'
+import { View, Text } from 'react-native'
+import { Icon } from 'react-native-elements'
 
 const TabStack = createBottomTabNavigator()
+const BoatStack = createStackNavigator()
+
+const BoatNavigator = () => {
+  const { Navigator, Screen } = BoatStack
+
+  return (
+    <Navigator initialRouteName="BoatsList" headerMode="none">
+      <Screen name="BoatsList" component={Boats} />
+      <Screen name="BoatInformation" component={BoatInformation} />
+    </Navigator>
+  )
+}
+
+interface TabContainerProps {
+  label?: string
+  focused?: boolean
+}
+
+const TabContainer: React.FunctionComponent<TabContainerProps> = ({
+  children,
+  label,
+  focused,
+}) => (
+  <>
+    {focused ? (
+      <View
+        style={{
+          borderTopWidth: 1,
+          borderTopColor: 'green',
+          alignItems: 'center',
+          flex: 1,
+          paddingTop: 4,
+        }}
+      >
+        {children}
+        <Text style={{ color: '#31AAB7', marginTop: 6 }}>{label}</Text>
+      </View>
+    ) : (
+      <View
+        style={{ width: '100%', alignItems: 'center', flex: 1, paddingTop: 5 }}
+      >
+        {children}
+        <Text style={{ color: '#ACBAC3', marginTop: 6 }}>{label}</Text>
+      </View>
+    )}
+  </>
+)
 
 const TabNavigator = () => {
   const { Navigator, Screen } = TabStack
 
   return (
-    <Navigator>
+    <Navigator
+      initialRouteName="Home"
+      tabBarOptions={{
+        showLabel: false,
+        style: {
+          height: 80,
+          backgroundColor: '#30444E',
+          borderTopColor: 'rgba(0, 0, 0, 0)',
+        },
+      }}
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused }) => {
+          let label, iconName: string
+
+          switch (route.name) {
+            case 'Home':
+              label = 'Home'
+              iconName = 'home'
+              break
+            case 'Terminology':
+              label = 'Terminology'
+              iconName = 'book'
+              break
+            case 'Boats':
+              label = 'Boats'
+              iconName = 'ship'
+              break
+            case 'Settings':
+              label = 'Settings'
+              iconName = 'gear'
+              break
+            default:
+              return null
+          }
+          return (
+            <TabContainer label={label} focused={focused}>
+              <Icon
+                type="font-awesome"
+                name={iconName}
+                color={focused ? '#31AAB7' : '#ACBAC3'}
+              />
+            </TabContainer>
+          )
+        },
+      })}
+    >
       <Screen name="Home" component={Home} />
-      <Screen name="Thing1" component={Thing1} />
-      <Screen name="Thing2" component={Thing2} />
+      <Screen name="Terminology" component={Terminology} />
+      <Screen name="Boats" component={BoatNavigator} />
       <Screen name="Settings" component={Settings} />
     </Navigator>
   )
--- app/types.ts
+++ app/types.ts
@@ -1,7 +1,20 @@
+import { NavigatorScreenParams } from '@react-navigation/native'
+import { Image, ImageURISource } from 'react-native'
+
 export type TabNavigationStackParamsList = {
   Tabs: undefined
   Home: undefined
-  Thing1: undefined
-  Thing2: undefined
+  Terminology: undefined
+  Boats: NavigatorScreenParams<BoatsParamList>
   Settings: undefined
+}
+
+export type BoatsParamList = {
+  BoatsList: undefined
+  BoatInformation: {
+    name: string
+    text: string
+    image: ImageURISource
+    credit: string
+  }
 }
