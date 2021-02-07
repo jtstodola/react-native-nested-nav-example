83346bcebfa6a90e4aecd608392bfa90755a3bde
Jess Stodola <jtstodola@gmail.com>
2021-02-06 20:00:35 -0600

    Add boat images

--- app/assets/images/index.ts
+++ app/assets/images/index.ts
@@ -1,11 +1,37 @@
 import background1 from './background1.jpg'
 import background2 from './background2.jpg'
 import background3 from './background3.jpg'
+import bowRider from './bowRider.jpg'
+import cabinCruiser from './cabinCruiser.jpg'
+import catamaran from './catamaran.jpg'
+import centerConsoleBoat from './centerConsoleBoat.jpg'
+import cuddyCabinBoat from './cuddyCabinBoat.jpg'
+import davidCohenDingyBoats from './davidCohenDingyBoats.jpg'
+import deckBoat from './deckBoat.jpg'
+import dinghyBoat from './dinghyBoat.jpg'
+import fishingBoat from './fishingBoat.jpg'
+import gameBoat from './gameBoat.jpg'
+import houseBoat from './houseboat.jpg'
+import motorYacht from './motorYacht.jpg'
+import trawlerBoat from './trawlerBoat.jpg'
 
 const images = {
   background1,
   background2,
   background3,
+  bowRider,
+  cabinCruiser,
+  catamaran,
+  centerConsoleBoat,
+  cuddyCabinBoat,
+  davidCohenDingyBoats,
+  deckBoat,
+  dinghyBoat,
+  fishingBoat,
+  gameBoat,
+  houseBoat,
+  motorYacht,
+  trawlerBoat,
 }
 
 export default images
